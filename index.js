a3dbd9c9aa916614327163aaedcef23bb4017282
Matthew Hallquist <matthallquist@yahoo.com>
2018-06-29 13:43:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,6 +5,5 @@ function findMatching(array, condition) {
 
 function fuzzyMatch(array, partialName) {
   partialName = partialName.split("");
-  return partialName.count;
-  return partialName;
+  return partialName
 }
