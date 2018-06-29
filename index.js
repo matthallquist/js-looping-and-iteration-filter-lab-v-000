0cab28e61717acd0e00bd8ea13bf073cb19f3785
Matthew Hallquist <matthallquist@yahoo.com>
2018-06-29 13:29:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 // Code your solution in this file
-const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
 
 function findMatching(array, condition) {
   array.filter(instance => alert(instance === condition));
