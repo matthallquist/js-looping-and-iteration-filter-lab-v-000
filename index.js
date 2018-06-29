8a22ce55d84232b3254e55a8aed4362d8dbbca7b
Matthew Hallquist <matthallquist@yahoo.com>
2018-06-29 13:31:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 // Code your solution in this file
 function findMatching(array, condition) {
-  return array.filter(instance => instance === condition);
+  return array.filter(instance => instance.toLowerCase() === condition.toLowerCase());
 }
