3ae7185b0792d71e431943930a4b623a4adfb357
Matthew Hallquist <matthallquist@yahoo.com>
2018-06-29 13:25:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Code your solution in this file
 const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
 
-function findMatching(drivers) {
-
+function findMatching(name) {
+  drivers.filter(function (driver) {return driver === name})
 }
