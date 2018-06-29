f958696515b02b0a9fd8fa14d1ea4804adc8ed67
Matthew Hallquist <matthallquist@yahoo.com>
2018-06-29 14:03:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,6 +10,6 @@ function fuzzyMatch(array, partialName) {
   for (const instance of array) {
     newArray.push(instance.split(""));
   }
-  return newArray.filter(instance => instance[0, letterQty] === partialName);
-  
+  newArray.filter(instance => instance[0, letterQty] === partialName);
+
 }
